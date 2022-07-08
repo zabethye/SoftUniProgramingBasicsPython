facb05c34999e7018a603396c2bd4d3d4e5df6c3
Zabethye <74608839+zabethye@users.noreply.github.com>
2022-07-09 00:47:00 +0300

    Create projects_creation.py
    Program that concatenates and calculate how many hours will need one architect to make n projects, where the name of the architect and number of projects are inputs. One project can be done in 3 hours.

---
+++ projects_creation.py
@@ -0,0 +1,5 @@
+architect = input()
+number_projects = int(input())
+needed_hours = number_projects * 3
+
+print(f"The architect {architect} will need {needed_hours} hours to complete {number_projects} project/s.")
